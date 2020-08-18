60e6c4f7fdadd3942726f025ae6da06af2e73706
mingyi456 <mingyi456@hotmail.com>
2020-08-19 02:15:37 +0800

    Massive cleanup and removal of old code

--- Prototype 3/move.py
+++
@@ -1,5 +0,0 @@
-self.sp.rect[0]= min(400, self.sp.rect[0] + 5)
-if self.forceDone:
-	self.sp.rect[0]= 400
-if self.sp.rect[0] < 400:
-	self.scriptsDone.append(False)
--- keytest.py
+++ keytest.py
@@ -10,16 +10,21 @@ time = str(datetime.timedelta(seconds=time_elapsed))
 pygame.display.set_mode((600, 400))
 
 while True:
-    keys = pygame.key.get_pressed()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
-            print(event.scancode, event.key, time, pygame.key.name(event.key))
-        if event.type == KEYUP:
-            print(event.scancode, event.key, time, pygame.key.name(event.key))
+	keys = pygame.key.get_pressed()
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == KEYDOWN:
+			print(format(pygame.key.get_mods(), 'b'))
 
-    time_elapsed += fpsClock.get_time()
-    time = str(datetime.timedelta(milliseconds=time_elapsed))
-    fpsClock.tick(60)
+# =============================================================================
+# 			print(event.scancode, event.key, time, pygame.key.name(event.key))
+# 		if event.type == KEYUP:
+# 			print(event.scancode, event.key, time)
+# 
+# =============================================================================
+
+	time_elapsed += fpsClock.get_time()
+	time = str(datetime.timedelta(milliseconds=time_elapsed))
+	fpsClock.tick(60)
